857f4963d7cd53d55510303c57b650a8fa5a38b6
devpiush <dev.bosepiush@gmail.com>
2025-04-17 23:29:56 +0530

    code: added ability to react to the messages

--- src/handlers/message-handler.ts
+++ src/handlers/message-handler.ts
@@ -1,4 +1,4 @@
-import { Message } from "discord.js";
+import { Message, MessageFlags } from "discord.js";
 import { saveUpdateToDB } from "../services/db";
 import { CONFIG } from "../config/config";
 
@@ -6,6 +6,13 @@ export const handleMessage = async (message: Message) => {
   if (message.channelId !== CONFIG.UPDATE_CHANNEL_ID || message.author.bot)
     return;
   try {
+    await message.react("✅");
+
+    await message.reply({
+      content:
+        "Your update has been recorded! Thank you for your contribution.",
+    });
+
     await saveUpdateToDB(
       message.author.id,
       message.author.username,
